825c905e58898ed9b9c139ce809431783e9caf84
Steve99Holmes <stevenhholmes@yahoo.com>
2024-06-24 07:51:45 -0400

    react exercises 2 events

--- react-exercises/src/App.tsx
+++ react-exercises/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './components/ComponentOne.css';
+// import './components/ComponentOne.css';
+import './components/Events.css';
 
-import ComponentOne from './components/ComponentOne';
-
+// import ComponentOne from './components/ComponentOne';
+import Events from './components/Events';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ComponentOne />
+        {/* <ComponentOne /> */}
+        <Events />
       </header>
     </div>
   );
---
+++ react-exercises/src/components/Events.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Events.css';
+
+const Events = () => {
+
+    const inputValueChanged = (event: any) => { console.log("input_field value =", event.target.value);} 
+
+    const inputButtonClicked = () => { console.log("input_button clicked!") };
+
+    return (
+        <div className = "input_container">
+            <input className = "input_field" onChange={inputValueChanged} type="input" placeholder="Enter something here" />
+            <br />
+            <button className = "input_button" onClick={inputButtonClicked}  type = "submit">Do it</button>      
+        </div>
+    )
+}
+export default Events;
+ 
